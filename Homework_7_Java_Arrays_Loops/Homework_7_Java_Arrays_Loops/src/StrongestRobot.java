6b886c6b3e785d7a5bd294ff3646d06b46662d99
IrinaPleshakova <irynapleshakova@gmail.com>
2024-06-28 22:26:01 +0200

    Update StrongestRobot.java

--- Homework_7_Java_Arrays_Loops/Homework_7_Java_Arrays_Loops/src/StrongestRobot.java
+++ Homework_7_Java_Arrays_Loops/Homework_7_Java_Arrays_Loops/src/StrongestRobot.java
@@ -12,10 +12,10 @@ public class StrongestRobot {
     public static void main(String[] args) {
 
         int[] weights = {300, 450, 200, 500, 350};
-        int maxWeight = weights[0];
-        int strongestRobot = 1;
+        int maxWeight = 0;
+        int strongestRobot = 0;
 
-        for (int i = 1; i < weights.length; i++) {
+        for (int i = 0; i < weights.length; i++) {
             if (weights[i] > maxWeight) {
                 maxWeight = weights[i];
                 strongestRobot = i + 1;
