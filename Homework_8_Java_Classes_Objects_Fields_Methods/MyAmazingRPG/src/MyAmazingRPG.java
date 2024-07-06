d5b7987ea96c01bd19ce841c6e6a8706c0eb4c47
IrinaPleshakova <irynapleshakova@gmail.com>
2024-07-06 21:11:16 +0200

    Update MyAmazingRPG.java
    1. Added list of heroes and list of enemies
    2. Added random hero and enemy selection

--- Homework_8_Java_Classes_Objects_Fields_Methods/MyAmazingRPG/src/MyAmazingRPG.java
+++ Homework_8_Java_Classes_Objects_Fields_Methods/MyAmazingRPG/src/MyAmazingRPG.java
@@ -1,40 +1,66 @@
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Random;
 
 public class MyAmazingRPG {
     public static void main(String[] args) {
-        Character codeWarrior = new Character("Кодовый Воитель", 120, 30, 15); //герой
-        Character bugHunter = new Character("Охотник за Багами", 100, 20, 10); //герой
+        //Создаю список героев
+        ArrayList<Character> heroes = new ArrayList<>();
+        Character codeWarrior = new Character("Code Warrior", 120, 30, 15);
+        Character bugHunter = new Character("Bug Hunter", 100, 20, 10);
+        Character serverSavior = new Character("Server Savior", 90, 25, 20);
+        Character pixelMage = new Character("Pixel Mage", 90, 20, 10);
+        Character taskMaster = new Character("Task Master", 80, 30, 50);
+        heroes.add(codeWarrior);
+        heroes.add(bugHunter);
+        heroes.add(serverSavior);
+        heroes.add(pixelMage);
+        heroes.add(taskMaster);
 
+        //Создаю список врагов
+        ArrayList<Character> enemies = new ArrayList<>();
+        Character juniorBug = new Character("Junior Bug", 80, 10, 5);
+        Character codeCrasher = new Character("Code Crasher", 90, 20, 10);
+        Character legacyCodeMonster = new Character("Legacy Code Monster", 70, 40, 20);
+        Character nullPointerException = new Character("Null Pointer Exception", 80, 20, 20);
+        enemies.add(juniorBug);
+        enemies.add(codeCrasher);
+        enemies.add(legacyCodeMonster);
+        enemies.add(nullPointerException);
 
-        Character juniorBug = new Character("Юный Баг", 80, 10, 5); //враг
-        Character codeCrasher = new Character("Кодолом", 90, 20, 10); //враг
+        Random random = new Random();
+        Character hero = heroes.get(random.nextInt(heroes.size()));
+        Character enemy = enemies.get(random.nextInt(enemies.size()));
 
-        System.out.println("Исходное состояние персонажей:");
-        codeWarrior.displayStatus();
-        bugHunter.displayStatus();
-        juniorBug.displayStatus();
-        codeCrasher.displayStatus();
+        System.out.println("Выбранный герой: ");
+        hero.displayStatus();
+        System.out.println("Выбранный противник: ");
+        enemy.displayStatus();
 
-        System.out.println("\nНачало битвы между " + codeWarrior.name + " и " + juniorBug.name + "\n");
+//        System.out.println("\nИсходное состояние персонажей:");
+//        codeWarrior.displayStatus();
+//        bugHunter.displayStatus();
+//        juniorBug.displayStatus();
+//        legacyCodeMonster.displayStatus();
 
-        while (codeWarrior.checkLifeStatus() && juniorBug.checkLifeStatus()) {
-            codeWarrior.attack(juniorBug);
-            if (juniorBug.checkLifeStatus()) {
-                juniorBug.attack(codeWarrior);
+        System.out.println("\nНачало битвы между " + hero.name + " и " + enemy.name + "\n");
+
+        while (hero.checkLifeStatus() && enemy.checkLifeStatus()) {
+            hero.attack(enemy);
+            if (enemy.checkLifeStatus()) {
+                enemy.attack(hero);
             }
 
             System.out.println("\nСостояние персонажей после атаки: ");
-            codeWarrior.displayStatus();
-            juniorBug.displayStatus();
+            hero.displayStatus();
+            enemy.displayStatus();
 
         }
 
-        if (codeWarrior.isAlive) {
-            System.out.println("\n" + codeWarrior.name + " победил!");
+        if (hero.isAlive) {
+            System.out.println("\n" + hero.name + " победил!");
         }
         else {
-            System.out.println("\n" + juniorBug.name + " победил!");
+            System.out.println("\n" + enemy.name + " победил!");
 
         }
         System.out.println("\nБитва завершена!");
