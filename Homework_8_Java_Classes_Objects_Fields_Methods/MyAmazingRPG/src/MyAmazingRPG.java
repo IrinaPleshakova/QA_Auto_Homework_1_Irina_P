7278768fc2795d6b13e5fdc6ae7055368855d90a
IrinaPleshakova <irynapleshakova@gmail.com>
2024-07-06 23:24:33 +0200

    Update MyAmazingRPG.java
    1. Added a loop that runs through the hero and enemy lists, randomly selecting pairs for battle until all participants are engaged.
    2. Added removing the loser from the list after a battle.
    3. Added output of a message about the winning side
    4. Added javadoc describing the game mechanics

--- Homework_8_Java_Classes_Objects_Fields_Methods/MyAmazingRPG/src/MyAmazingRPG.java
+++ Homework_8_Java_Classes_Objects_Fields_Methods/MyAmazingRPG/src/MyAmazingRPG.java
@@ -1,6 +1,18 @@
 import java.util.ArrayList;
 import java.util.Random;
 
+/**
+ * Механика игры:
+ * 1. Пока есть живые герои и враги, случайным образом выбирается герой и враг для сражения.
+ * 2. Выводится информация о выбранных персонажах.
+ * 3. Проходит битва между выбранными персонажами, пока один из них не умрет. Во время битвы каждый персонаж поочередно
+ * атакует другого, и после каждой атаки выводится текущее состояние персонажей.
+ * 4. Значение здоровья у победившего персонажа не восстанавливается (т.е. в следующем бою этот персонаж будет иметь
+ * столько здоровья, сколько у него осталось после предыдущей битвы).
+ * Результаты битвы:
+ * 1. После завершения битвы проверяется, кто победил. Проигравший удаляется из списка героев или врагов.
+ * 2. Если герои или враги закончились, выводится соответствующее сообщение о победе или поражении.
+ */
 public class MyAmazingRPG {
     public static void main(String[] args) {
         //Создаю список героев
@@ -18,40 +30,57 @@ public class MyAmazingRPG {
         enemies.add(new Character("Legacy Code Monster", 70, 40, 20));
         enemies.add(new Character("Null Pointer Exception", 80, 20, 20));
 
+        System.out.println("Общее количество созданных персонажей: " + Character.checkCharacterCount());
+
         Random random = new Random();
-        Character hero = heroes.get(random.nextInt(heroes.size()));
-        Character enemy = enemies.get(random.nextInt(enemies.size()));
+        while (heroes.size() > 0 && enemies.size() > 0) {
+            Character hero = heroes.get(random.nextInt(heroes.size()));
+            Character enemy = enemies.get(random.nextInt(enemies.size()));
 
-        System.out.println("Выбранный герой: ");
-        hero.displayStatus();
-        System.out.println("Выбранный противник: ");
-        enemy.displayStatus();
-
-        System.out.println("\nНачало битвы между " + hero.name + " и " + enemy.name + "\n");
-
-        while (hero.checkLifeStatus() && enemy.checkLifeStatus()) {
-            hero.attack(enemy);
-            System.out.println("\nСостояние персонажей после атаки героя: ");
+            System.out.println("----------------------------------------------------------------");
+            System.out.println("\nВыбранный герой: ");
             hero.displayStatus();
+            System.out.println("Выбранный противник: ");
             enemy.displayStatus();
 
-            if (enemy.checkLifeStatus()) {
-                enemy.attack(hero);
-                System.out.println("\nСостояние персонажей после атаки противника: ");
+            System.out.println("\nНачало битвы между " + hero.name + " и " + enemy.name + "\n");
+
+            while (hero.checkLifeStatus() && enemy.checkLifeStatus()) {
+                hero.attack(enemy);
+                System.out.println("\nСостояние персонажей после атаки героя: ");
                 hero.displayStatus();
                 enemy.displayStatus();
+
+                if (enemy.checkLifeStatus()) {
+                    enemy.attack(hero);
+                    System.out.println("\nСостояние персонажей после атаки противника: ");
+                    hero.displayStatus();
+                    enemy.displayStatus();
+                }
             }
 
-        }
+            System.out.println("\nБитва завершена!");
 
-        if (hero.isAlive) {
-            System.out.println("\n" + hero.name + " победил!");
+            if (hero.isAlive) {
+                System.out.println("\n" + hero.name + " победил!");
+            } else {
+                System.out.println("\n" + enemy.name + " победил!");
+
+            }
+            if (!hero.isAlive) {
+                System.out.println("\n" + hero.name + " проиграл и будет удален из списка героев!");
+                heroes.remove(hero);
+            } else {
+                System.out.println("\n" + enemy.name + " проиграл и будет удален из списка врагов!");
+                enemies.remove(enemy);
+            }
+        }
+        if (heroes.size() > 0) {
+            System.out.println("\nПОЗДРАВЛЯЮ! Герои победили!");
         }
         else {
-            System.out.println("\n" + enemy.name + " победил!");
-
+            System.out.println("\nОЧЕНЬ ЖАЛЬ! Монстры в этот раз были круче!");
+            System.out.println("\nИДИ ПОДКАЧАЙСЯ :Р и приходи снова :)");
         }
-        System.out.println("\nБитва завершена!");
-        System.out.println("Общее количество созданных персонажей: " + Character.checkCharacterCount());
     }
 }
