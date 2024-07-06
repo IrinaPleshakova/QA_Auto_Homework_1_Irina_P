c11d01c6528376f4d94f925bfba7bec5acd5b041
IrinaPleshakova <irynapleshakova@gmail.com>
2024-07-06 21:50:19 +0200

    Update MyAmazingRPG.java
    Optimized piece of code with heroes and enemies lists

--- Homework_8_Java_Classes_Objects_Fields_Methods/MyAmazingRPG/src/MyAmazingRPG.java
+++ Homework_8_Java_Classes_Objects_Fields_Methods/MyAmazingRPG/src/MyAmazingRPG.java
@@ -5,27 +5,18 @@ public class MyAmazingRPG {
     public static void main(String[] args) {
         //Создаю список героев
         ArrayList<Character> heroes = new ArrayList<>();
-        Character codeWarrior = new Character("Code Warrior", 120, 30, 15);
-        Character bugHunter = new Character("Bug Hunter", 100, 20, 10);
-        Character serverSavior = new Character("Server Savior", 90, 25, 20);
-        Character pixelMage = new Character("Pixel Mage", 90, 20, 10);
-        Character taskMaster = new Character("Task Master", 80, 30, 50);
-        heroes.add(codeWarrior);
-        heroes.add(bugHunter);
-        heroes.add(serverSavior);
-        heroes.add(pixelMage);
-        heroes.add(taskMaster);
+        heroes.add(new Character("Code Warrior", 120, 30, 15));
+        heroes.add(new Character("Bug Hunter", 100, 20, 10));
+        heroes.add(new Character("Server Savior", 90, 25, 20));
+        heroes.add(new Character("Pixel Mage", 90, 20, 10));
+        heroes.add(new Character("Task Master", 80, 30, 50));
 
         //Создаю список врагов
         ArrayList<Character> enemies = new ArrayList<>();
-        Character juniorBug = new Character("Junior Bug", 80, 10, 5);
-        Character codeCrasher = new Character("Code Crasher", 90, 20, 10);
-        Character legacyCodeMonster = new Character("Legacy Code Monster", 70, 40, 20);
-        Character nullPointerException = new Character("Null Pointer Exception", 80, 20, 20);
-        enemies.add(juniorBug);
-        enemies.add(codeCrasher);
-        enemies.add(legacyCodeMonster);
-        enemies.add(nullPointerException);
+        enemies.add(new Character("Junior Bug", 80, 10, 5));
+        enemies.add(new Character("Code Crasher", 90, 20, 10));
+        enemies.add(new Character("Legacy Code Monster", 70, 40, 20));
+        enemies.add(new Character("Null Pointer Exception", 80, 20, 20));
 
         Random random = new Random();
         Character hero = heroes.get(random.nextInt(heroes.size()));
@@ -35,12 +26,6 @@ public class MyAmazingRPG {
         hero.displayStatus();
         System.out.println("Выбранный противник: ");
         enemy.displayStatus();
-
-//        System.out.println("\nИсходное состояние персонажей:");
-//        codeWarrior.displayStatus();
-//        bugHunter.displayStatus();
-//        juniorBug.displayStatus();
-//        legacyCodeMonster.displayStatus();
 
         System.out.println("\nНачало битвы между " + hero.name + " и " + enemy.name + "\n");
 
