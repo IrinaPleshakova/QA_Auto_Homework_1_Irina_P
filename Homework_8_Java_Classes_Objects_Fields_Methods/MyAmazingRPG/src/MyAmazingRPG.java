1fe40d486a947ca07ef45eb93eb982b68e136233
IrinaPleshakova <irynapleshakova@gmail.com>
2024-07-07 10:02:08 +0200

    Update MyAmazingRPG.java
    Added battle with super monster

--- Homework_8_Java_Classes_Objects_Fields_Methods/MyAmazingRPG/src/MyAmazingRPG.java
+++ Homework_8_Java_Classes_Objects_Fields_Methods/MyAmazingRPG/src/MyAmazingRPG.java
@@ -9,9 +9,11 @@ import java.util.Random;
  * атакует другого, и после каждой атаки выводится текущее состояние персонажей.
  * 4. Значение здоровья у победившего персонажа не восстанавливается (т.е. в следующем бою этот персонаж будет иметь
  * столько здоровья, сколько у него осталось после предыдущей битвы).
+ * 5. Если герои побеждают, то последний герой должен сразится с супермонстром.
  * Результаты битвы:
  * 1. После завершения битвы проверяется, кто победил. Проигравший удаляется из списка героев или врагов.
  * 2. Если герои или враги закончились, выводится соответствующее сообщение о победе или поражении.
+ * 3. После битвы с супермонстром выводится сообщение о победе или поражении.
  */
 public class MyAmazingRPG {
     public static void main(String[] args) {
@@ -30,8 +32,12 @@ public class MyAmazingRPG {
         enemies.add(new Character("Legacy Code Monster", 70, 40, 20));
         enemies.add(new Character("Null Pointer Exception", 80, 20, 20));
 
-        System.out.println("Общее количество созданных персонажей: " + Character.checkCharacterCount());
+        //Создаю супермонстра - босса
+        Character darkOverlord = new Character("Dark Overlord", 200, 50, 20);
 
+
+        System.out.println("Общее количество созданных персонажей: " + Character.checkCharacterCount());
+        Character lastHero = null;
         Random random = new Random();
         while (heroes.size() > 0 && enemies.size() > 0) {
             Character hero = heroes.get(random.nextInt(heroes.size()));
@@ -63,6 +69,7 @@ public class MyAmazingRPG {
 
             if (hero.isAlive) {
                 System.out.println("\n" + hero.name + " победил!");
+                lastHero = hero;
             } else {
                 System.out.println("\n" + enemy.name + " победил!");
 
@@ -77,10 +84,36 @@ public class MyAmazingRPG {
         }
         if (heroes.size() > 0) {
             System.out.println("\nПОЗДРАВЛЯЮ! Герои победили!");
+            System.out.println("ТЕПЕРЬ ВАМ ДОСТУПНА БИТВА С СУПЕРМОНСТРОМ!!!");
+            System.out.println("\nПоследний выживший герой: " + lastHero.name);
+            System.out.println("\nБитва с супермонстром: " + darkOverlord.name);
+
+            while (lastHero.checkLifeStatus() && darkOverlord.checkLifeStatus()) {
+                lastHero.attack(darkOverlord);
+                System.out.println("\nСостояние персонажей после атаки героя: ");
+                lastHero.displayStatus();
+                darkOverlord.displayStatus();
+                if (darkOverlord.checkLifeStatus()) {
+                    darkOverlord.attack(lastHero);
+                    System.out.println("\nСостояние персонажей после атаки супер монстра: ");
+                    lastHero.displayStatus();
+                    darkOverlord.displayStatus();
+                }
+            }
+            System.out.println("\nФинальная битва завершена!");
+
+            if (lastHero.isAlive) {
+                System.out.println("\n" + lastHero.name + " победил супер монстра!");
+                System.out.println("\nПОЗДРАВЛЯЕМ! Герои одержали окончательную победу!");
+                System.out.println("Ты невероятно крут!!!");
+            } else {
+                System.out.println("\n" + darkOverlord.name + " победил последнего героя!");
+                System.out.println("\nМОНСТРЫ ОДЕРЖАЛИ ОКОНЧАТЕЛЬНУЮ ПОБЕДУ!!! *рыдающий смайл*");
+            }
         }
-        else {
+        else{
             System.out.println("\nОЧЕНЬ ЖАЛЬ! Монстры в этот раз были круче!");
-            System.out.println("\nИДИ ПОДКАЧАЙСЯ :Р и приходи снова :)");
+            System.out.println("ИДИ ПОДКАЧАЙСЯ :Р и приходи снова :)");
         }
     }
 }
