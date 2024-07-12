672084a9ca491ec2b6fb3ba64588703459bc8119
IrinaPleshakova <irynapleshakova@gmail.com>
2024-07-12 16:52:36 +0200

    Create class Employee.java
    1. Add getters and setters for name, age and salary fields.
    2. Create work, takeBreak, performJob and describeRole methods.

---
+++ Homework_9_Java_Encapsulation_Inheritance_Polymorphism/DreamTeamInc/src/com/example/employees/Employee.java
@@ -0,0 +1,54 @@
+package com.example.employees;
+
+public class Employee {
+	private String name;
+	private int age;
+	private double salary;
+
+	public Employee (String name, int age, double salary) {
+		this.name = name;
+		this.age = age;
+		this.salary = salary;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	public void work() {
+		System.out.println(name + " is working.");
+	}
+
+	public void takeBreak() {
+		System.out.println(name + "is taking a break.");
+	}
+
+	public void performJob() {
+		System.out.println(name + "is performing assigned job");
+	}
+
+	public void describeRole() {
+		System.out.println(name + "is an employee with a salary of " + salary);
+	}
+
+}
