22c50bd96b04889180a5a633176bb28dc6182244
IrinaPleshakova <irynapleshakova@gmail.com>
2024-07-12 19:09:30 +0200

    Update Main.java
    1. Created ArrayList for Employee.
    2. Go over the employees list and called performJob and describeRole methods for each employee.

--- Homework_9_Java_Encapsulation_Inheritance_Polymorphism/DreamTeamInc/src/Main.java
+++ Homework_9_Java_Encapsulation_Inheritance_Polymorphism/DreamTeamInc/src/Main.java
@@ -1,4 +1,22 @@
+import com.example.employees.Designer;
+import com.example.employees.Developer;
+import com.example.employees.Employee;
+import com.example.employees.Manager;
+
+import java.util.ArrayList;
+
 public class Main {
 	public static void main(String[] args) {
-		
+
+		ArrayList<Employee> employees = new ArrayList<>();
+		employees.add(new Manager("Bartek", 25, 4000));
+		employees.add(new Developer("Konrad", 30, 3500));
+		employees.add(new Designer("Hanna", 27, 2500));
+
+		for (Employee employee: employees){
+			employee.performJob();
+			employee.describeRole();
+		}
+	}
+	
 }
