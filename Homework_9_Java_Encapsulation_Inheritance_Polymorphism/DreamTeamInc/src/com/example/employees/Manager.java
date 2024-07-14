657ac178e9d8330e9d2afc25dae83b400ea4e1fe
IrinaPleshakova <irynapleshakova@gmail.com>
2024-07-14 16:45:52 +0200

    Update Manager.java
    1. Added the getRole method to return "manager" as the role (override).
    2. Updated the describeRole methods to use getRole for consistent role descriptions.
    3. Added the assignTask method to allow managers to assign tasks to employees.
    4. Added override participateInMeeting and takeBreakAfterMeeting methods.
    5. Added the conductMeeting method to simulate managers conducting meetings.

--- Homework_9_Java_Encapsulation_Inheritance_Polymorphism/DreamTeamInc/src/com/example/employees/Manager.java
+++ Homework_9_Java_Encapsulation_Inheritance_Polymorphism/DreamTeamInc/src/com/example/employees/Manager.java
@@ -1,9 +1,18 @@
 package com.example.employees;
 
+/**
+ * Represents a manager with specific attributes and methods.
+ */
+
 public class Manager extends Employee {
 
 	public Manager(String name, int age, double salary, String department) {
 		super(name, age, salary, department);
+	}
+
+	@Override
+	public String getRole() {
+		return "manager";
 	}
 
 	@Override
@@ -13,12 +22,30 @@ public class Manager extends Employee {
 
 	@Override
 	public void describeRole() {
-		System.out.println(getName() + " is a manager with a salary of " + getSalary());
+		System.out.println(getName() + " is a " + getRole() + " with a salary of " + getSalary());
 	}
 
 	@Override
 	public void describeRole(String extraInfo) {
-		System.out.println(getName() + " is a manager in the " + getDepartment() + " department.");
+		System.out.println(getName() + " is a " + getRole() + " in the " + getDepartment() + " department.");
 	}
 
+	public void assignTask(Employee employee, String task) {
+		System.out.println(getName() + " is assigning task: " + task + " to " + employee.getName());
+		employee.performJob();
+	}
+
+	@Override
+	public void participateInMeeting() {
+		conductMeeting();
+	}
+
+	@Override
+	public void takeBreakAfterMeeting() {
+		takeBreak();
+	}
+
+	public void conductMeeting() {
+		System.out.println(getName() + " is conducting meeting.");
+	}
 }
