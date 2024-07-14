34151c434b8ff385a6dc5e5abe09f25cbc1fae4a
IrinaPleshakova <irynapleshakova@gmail.com>
2024-07-14 16:48:54 +0200

    Update Developer.java
    1. Added the getRole method to return "developer" as the role (override).
    2. Updated the describeRole methods to use getRole for consistent role descriptions.
    3. Added override participateInMeeting and takeBreakAfterMeeting methods.

--- Homework_9_Java_Encapsulation_Inheritance_Polymorphism/DreamTeamInc/src/com/example/employees/Developer.java
+++ Homework_9_Java_Encapsulation_Inheritance_Polymorphism/DreamTeamInc/src/com/example/employees/Developer.java
@@ -1,9 +1,18 @@
 package com.example.employees;
 
+/**
+ * Represents a developer with specific attributes and methods.
+ */
+
 public class Developer extends Employee {
 
 	public Developer(String name, int age, double salary, String department) {
 		super(name, age, salary, department);
+	}
+
+	@Override
+	public String getRole() {
+		return "developer";
 	}
 
 	@Override
@@ -13,11 +22,21 @@ public class Developer extends Employee {
 
 	@Override
 	public void describeRole() {
-		System.out.println(getName() + " is a developer with a salary of " + getSalary());
+		System.out.println(getName() + " is a " + getRole() + " with a salary of " + getSalary());
 	}
 
 	@Override
 	public void describeRole(String extraInfo) {
-		System.out.println(getName() + " is a developer in the " + getDepartment() + " department."+ extraInfo);
+		System.out.println(getName() + " is a " + getRole() + " in the " + getDepartment() + " department."+ extraInfo);
+	}
+
+	@Override
+	public void participateInMeeting() {
+		System.out.println(getName() + " is participating in a meeting. ");
+	}
+
+	@Override
+	public void takeBreakAfterMeeting() {
+		takeBreak();
 	}
 }
