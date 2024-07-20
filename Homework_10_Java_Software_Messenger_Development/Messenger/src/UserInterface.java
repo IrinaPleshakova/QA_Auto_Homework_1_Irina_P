9fc624bc52945591f310ba000654ade6a0267639
IrinaPleshakova <irynapleshakova@gmail.com>
2024-07-20 23:32:51 +0200

    Create UserInterface.java
    - Created UserInterface with methods to get the user's name, contacts, and messages.
    - Added methods to addContact, sendMessage and receiveMessage.

---
+++ Homework_10_Java_Software_Messenger_Development/Messenger/src/UserInterface.java
@@ -0,0 +1,17 @@
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Interface representing a user in the messenger system.
+ */
+
+public interface UserInterface {
+
+	String getName();
+	Set<User> getContacts();
+	List<Message> getMessages();
+
+	void addContact(User contact);
+	void sendMessage(Message message);
+	void receiveMessage(Message message);
+}
