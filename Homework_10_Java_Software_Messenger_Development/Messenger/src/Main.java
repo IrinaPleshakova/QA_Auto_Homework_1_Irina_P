ff52502d1be5c65f0554bcb3c45f1558ae3bff42
IrinaPleshakova <irynapleshakova@gmail.com>
2024-07-20 23:47:53 +0200

    Create Main.java for testing
    - Created Main class to test the functionality of the messenger.
    - Created several users and added contacts for each user.
    - Sent messages between users.
    - Displayed the list of messages for each user along with their statuses.
    - Invoked the method to read messages from the perspective of the users who received the messages.
    - Verified and displayed the status of the messages after being read.

---
+++ Homework_10_Java_Software_Messenger_Development/Messenger/src/Main.java
@@ -0,0 +1,79 @@
+/**
+ * Main class for testing the messenger functionality.
+ */
+
+public class Main {
+	public static void main(String[] args) {
+		User hanna = new User("Hanna");
+		User bartek = new User("Bartek");
+		User konrad = new User("Konrad");
+
+		hanna.addContact(bartek);
+		hanna.addContact(konrad);
+		bartek.addContact(hanna);
+		konrad.addContact(hanna);
+
+		System.out.println("Hanna's contacts: ");
+		for (User contact : hanna.getContacts()) {
+			System.out.println(contact.getName());
+		}
+
+		System.out.println("\nBartek's contacts: ");
+		for (User contact : bartek.getContacts()) {
+			System.out.println(contact.getName());
+		}
+
+		System.out.println("\nKonrad's contacts: ");
+		for (User contact : konrad.getContacts()) {
+			System.out.println(contact.getName());
+		}
+
+		System.out.println();
+
+		Message message1 = new Message(hanna, bartek, "Hi, Bartek! ");
+		hanna.sendMessage(message1);
+
+		Message message2 = new Message(bartek,hanna, "Hi, Hanna :) ");
+		bartek.sendMessage(message2);
+
+		Message message3 = new Message(hanna,konrad, "Hi, Konrad! ");
+		hanna.sendMessage(message3);
+
+		System.out.println("\nHanna's messages: ");
+		for (Message message : hanna.getMessages()) {
+			System.out.println(message.getText() + "(Status: " + message.getStatus() + ")");
+		}
+
+		System.out.println("\nBartek's messages: ");
+		for (Message message : bartek.getMessages()) {
+			System.out.println(message.getText() + " (Status: " + message.getStatus() + ")");
+		}
+
+		System.out.println("\nKonrad's messages: ");
+		for (Message message : konrad.getMessages()) {
+			System.out.println(message.getText() + " (Status: " + message.getStatus() + ")");
+		}
+
+		System.out.println("\nReading messages...");
+		bartek.readMessage(message1);
+		hanna.readMessage(message2);
+		konrad.readMessage(message3);
+
+
+		System.out.println("\nHanna's messages after reading: ");
+		for (Message message : hanna.getMessages()) {
+			System.out.println(message.getText() + "(Status: " + message.getStatus() + ")");
+		}
+
+		System.out.println("\nBartek's messages after reading: ");
+		for (Message message : bartek.getMessages()) {
+			System.out.println(message.getText() + " (Status: " + message.getStatus() + ")");
+		}
+
+		System.out.println("\nKonrad's messages after reading: ");
+		for (Message message : konrad.getMessages()) {
+			System.out.println(message.getText() + " (Status: " + message.getStatus() + ")");
+		}
+
+	}
+}
